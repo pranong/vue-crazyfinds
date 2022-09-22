2bea9eb2e829d39af67231f097be07f68d32e1e3
pranong <nutpat2539@gmail.com>
2022-09-23 00:34:00 +0700

    new dynamic route

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,14 @@
   <v-container>
     <!-- Carousel -->
     <v-row>
-      <v-carousel cycle width="100%" show-arrows-on-hover hide-delimiter-background>
+      <v-carousel
+        cycle
+        width="100%"
+        show-arrows-on-hover
+        hide-delimiter-background
+      >
         <v-carousel-item
-          v-for="(item,i) in carouselItem"
+          v-for="(item, i) in carouselItem"
           :key="i"
           :src="item.src"
           transition="fade-transition"
@@ -15,9 +20,7 @@
     <!-- Devider -->
     <v-row>
       <v-col>
-        <v-divider
-            class="mt-5 mb-5"
-          ></v-divider>
+        <v-divider class="mt-5 mb-5"></v-divider>
       </v-col>
     </v-row>
 
@@ -26,7 +29,13 @@
       <v-col class="text-b">
         <marquee-text>
           <h1>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Arrival
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New
+            Arrival
           </h1>
         </marquee-text>
       </v-col>
@@ -39,22 +48,25 @@
           md="4"
           class="text-center"
         >
-          <v-container
-            class="pa-0 productItem"
-            cols="12"
-            @click="selectItem(row.id)"
-          >
+          <v-container class="pa-0 productItem" cols="12">
             <v-badge color="#BDBDBD" tile overlap offset-x="29" offset-y="25">
               <template v-slot:badge> Sale </template>
-              <v-carousel hide-delimiters :width="isMobile ? 150 : 250" :height="isMobile ? 150 : 250">
-                <v-carousel-item
-                  v-for="(item,i) in row.images"
-                  :key="i"
-                  :src="item.src"
-                  :height="isMobile ? 150 : 250"
+              <router-link :to="`details/${row.stkId}`">
+                <v-carousel
+                  hide-delimiters
                   :width="isMobile ? 150 : 250"
-                ></v-carousel-item>
-              </v-carousel>
+                  :height="isMobile ? 150 : 250"
+                  @click="window.scrollTo(0, 0)"
+                >
+                  <v-carousel-item
+                    v-for="(item, i) in row.images"
+                    :key="i"
+                    :src="item.src"
+                    :height="isMobile ? 150 : 250"
+                    :width="isMobile ? 150 : 250"
+                  ></v-carousel-item>
+                </v-carousel>
+              </router-link>
             </v-badge>
             <v-row class="pl-5 pr-5 pt-1">
               <v-col
@@ -62,11 +74,12 @@
                 class="text-caption text-left"
                 style="line-height: 140%; text-transform: uppercase"
               >
-                <b>{{row.name}}</b><br />
-                <em>${{row.priceA}}</em>
+                <b>{{ row.name }}</b
+                ><br />
+                <em>${{ row.priceA }}</em>
               </v-col>
               <v-col cols="2" class="text-body2">
-                {{row.size}}
+                {{ row.size }}
               </v-col>
             </v-row>
           </v-container>
@@ -76,17 +89,13 @@
 
     <!-- Shop more btn -->
     <v-row align="center" justify="center">
-      <v-btn class="mt-5 mb-5 cool-btn">
-        shop more
-      </v-btn>
+      <v-btn class="mt-5 mb-5 cool-btn"> shop more </v-btn>
     </v-row>
 
     <!-- Devider -->
     <v-row>
       <v-col>
-        <v-divider
-            class="mt-5 mb-5"
-          ></v-divider>
+        <v-divider class="mt-5 mb-5"></v-divider>
       </v-col>
     </v-row>
 
@@ -95,7 +104,9 @@
       <v-col class="text-b">
         <marquee-text>
           <h1>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shop Categories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shop Categories
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shop
+            Categories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shop
+            Categories
           </h1>
         </marquee-text>
         <!-- <h1>
@@ -110,30 +121,36 @@
         md="4"
         class="text-center"
       >
-        <v-container
-          class="pa-0 productItem"
-          cols="12"
-          @click="selectItem(row.id)"
-        >
+        <v-container class="pa-0 productItem" cols="12">
           <v-badge color="#BDBDBD" tile overlap offset-x="29" offset-y="25">
             <template v-slot:badge> Sale </template>
-            <v-carousel hide-delimiters :width="isMobile ? 150 : 250" :height="isMobile ? 150 : 250">
-              <v-carousel-item
-                v-for="(item,i) in row.images"
-                :key="i"
-                :src="item.src"
-                :height="isMobile ? 150 : 250"
+            <router-link :to="`product/${row.code}`">
+              <v-carousel
+                hide-delimiters
                 :width="isMobile ? 150 : 250"
-              ></v-carousel-item>
-            </v-carousel>
+                :height="isMobile ? 150 : 250"
+                :show-arrows="false"
+              >
+                <v-carousel-item
+                  v-for="(item, i) in row.images"
+                  :key="i"
+                  :src="item.src"
+                  :height="isMobile ? 150 : 250"
+                  :width="isMobile ? 150 : 250"
+                ></v-carousel-item>
+              </v-carousel>
+            </router-link>
           </v-badge>
           <v-row class="pl-5 pr-5 pt-1">
             <v-col
-              cols="10"
+              cols="12"
               class="text-caption text-left"
               style="line-height: 140%; text-transform: uppercase"
             >
-              <center><b>{{row.name}}</b><br /></center>
+              <center>
+                <b>{{ row.name }}</b
+                ><br />
+              </center>
             </v-col>
           </v-row>
         </v-container>
@@ -143,9 +160,7 @@
     <!-- Devider -->
     <v-row>
       <v-col>
-        <v-divider
-            class="mx-4 pt-5 pb-5"
-          ></v-divider>
+        <v-divider class="mx-4 pt-5 pb-5"></v-divider>
       </v-col>
     </v-row>
 
@@ -154,62 +169,63 @@
 </template>
 
 <script>
-  import MarqueeText from "../components/MarqueeText.vue";
-  export default {
-    name: 'Home',
-    components: {
-      MarqueeText,
-    },
-    data() {
-      return {
-        carouselItem: [],
-        rows: [],
-        masterRows: [],
+import MarqueeText from '../components/MarqueeText.vue';
+export default {
+  name: 'Home',
+  components: {
+    MarqueeText,
+  },
+  data() {
+    return {
+      carouselItem: [],
+      rows: [],
+      masterRows: [],
+    };
+  },
+  created() {
+    this.getApis();
+  },
+  computed: {
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return true;
+      } else {
+        return false;
       }
     },
-    created() {
-      window.scrollTo(0, 0)
-      this.getApis()
+  },
+  methods: {
+    setData(data) {
+      console.log('==>', data);
+      data.map((x) => (x.images = JSON.parse(x.images)));
+      this.masterRows = data;
+      this.rows = this.masterRows.slice(0, 4);
+      console.log('this.rows', this.rows);
     },
-    computed: {
-      isMobile() {
-        if (
-          /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-            navigator.userAgent
-          )
-        ) {
-          return true;
-        } else {
-          return false;
-        }
-      },
+    async selectItem(id) {
+      this.$router.push({ name: 'details', params: { id } });
     },
-    methods: {
-      setData (data) {
-        console.log('==>', data)
-        data.map(x => x.images = JSON.parse(x.images))
-        this.masterRows = data
-        this.rows = this.masterRows.slice(0, 4)
-        console.log('this.rows', this.rows)
-      },
-      async selectItem(id) {
-        this.$router.push({ name: "details", params: { id } });
-      },
-      async getApis() {
-        try {
-          let res = await this.$http.get('/stock/get-stock')
-          this.setData(res.data.items)
-          let { data } = await this.$http.post('/setting/get-setting',{ tableCode: ['CAROUSEL', 'CATEGORY']})
-          console.log(data.rows)
-          this.categoryItem = JSON.parse(data.rows[0].datas)
-          this.carouselItem = JSON.parse(data.rows[1].datas)
-          console.log('data', data)
-        } catch (error) {
-          console.log('ERR', error)
-        }
-      },
-    }
-  }
+    async getApis() {
+      try {
+        let res = await this.$http.get('/stock/get-stock');
+        this.setData(res.data.items);
+        let { data } = await this.$http.post('/setting/get-setting', {
+          tableCode: ['CAROUSEL', 'CATEGORY'],
+        });
+        console.log(data.rows);
+        this.categoryItem = JSON.parse(data.rows[0].datas);
+        this.carouselItem = JSON.parse(data.rows[1].datas);
+        console.log('data', this.categoryItem);
+      } catch (error) {
+        console.log('ERR', error);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="sass">
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/Home.vue";
-import details from "../views/Details.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/Home.vue';
+import details from '../views/Details.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: "/product",
-    name: "Shop",
+    path: '/product/:catType',
+    name: 'Shop',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/product.vue"),
+      import(/* webpackChunkName: "about" */ '../views/product.vue'),
   },
   {
-    path: "/details/:id",
-    name: "details",
+    path: '/details/:stkId',
+    name: 'details',
     component: details,
   },
 ];
